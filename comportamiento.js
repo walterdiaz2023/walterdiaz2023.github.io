3eabc83947e840842f542b29bc431472edfdb45f
walterdiaz2023 <108538677+walterdiaz2023@users.noreply.github.com>
2023-02-24 20:47:14 -0300

    Add files via upload

---
+++ comportamiento.js
@@ -0,0 +1,58 @@
+document.getElementById("boton1").addEventListener("click", myFunction1);
+document.getElementById("boton1").addEventListener("mouseout", myFunction1_1);
+document.getElementById("boton2").addEventListener("click", myFunction2);
+document.getElementById("boton2").addEventListener("mouseout", myFunction2_2);
+document.getElementById("boton3").addEventListener("click", myFunction3);
+document.getElementById("boton3").addEventListener("mouseout", myFunction3_3);
+document.getElementById("boton4").addEventListener("click", myFunction4);
+document.getElementById("boton4").addEventListener("mouseout", myFunction4_4);
+document.getElementById("boton5").addEventListener("click", myFunction5);
+document.getElementById("boton5").addEventListener("mouseout", myFunction5_5);
+document.getElementById("boton6").addEventListener("click", myFunction6);
+document.getElementById("boton6").addEventListener("mouseout", myFunction6_6);
+document.getElementById("boton7").addEventListener("click", myFunction7);
+document.getElementById("boton7").addEventListener("mouseout", myFunction7_7);
+
+function myFunction1() {
+    document.getElementById("informacion").style = "font-size:20px";
+}
+function myFunction1_1() {
+    document.getElementById("informacion").style = "font-size:14px";
+}
+function myFunction2() {
+    document.getElementById("idioma").style = "font-size:20px";
+}
+function myFunction2_2() {
+    document.getElementById("idioma").style = "font-size:14px";
+}
+function myFunction3() {
+    document.getElementById("experiencia").style = "font-size:20px";
+}
+function myFunction3_3() {
+    document.getElementById("experiencia").style = "font-size:14px";
+}
+function myFunction4() {
+    document.getElementById("formacion").style = "font-size:20px";
+}
+function myFunction4_4() {
+    document.getElementById("formacion").style = "font-size:14px";
+}
+function myFunction5() {
+    document.getElementById("presentacion").style = "font-size:20px";
+}
+function myFunction5_5() {
+    document.getElementById("presentacion").style = "font-size:14px";
+}
+function myFunction6() {
+    document.getElementById("habilidades").style = "font-size:20px";
+}
+function myFunction6_6() {
+    document.getElementById("habilidades").style = "font-size:14px";
+}
+function myFunction7() {
+    document.getElementById("redes").style = "font-size:20px";
+}
+function myFunction7_7() {
+    document.getElementById("redes").style = "font-size:14px";
+}
+
